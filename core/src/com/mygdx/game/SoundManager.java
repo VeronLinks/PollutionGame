6b46d330b1050c762f3210b121d13de9df21bf98
luis <lusor97@gmail.com>
2019-05-16 17:41:35 +0200

    añadidos sonidos por codigo, falta comprovar si van

--- core/src/com/mygdx/game/SoundManager.java
+++ core/src/com/mygdx/game/SoundManager.java
@@ -7,12 +7,18 @@ public class SoundManager
 {
     private static SoundManager instance;
 
-    public Sound cardUsed,addPolution;
+    public Sound click,badCard1,badCard2,goodCard1,goodCard2,mainTheme1,mainTheme2,mainMenu;
 
     private SoundManager()
     {
-        cardUsed= Gdx.audio.newSound(Gdx.files.internal("Jump.wav"));
-        //addPolution= Gdx.audio.newSound(Gdx.files.internal("Tonto.wav"));
+        click = Gdx.audio.newSound(Gdx.files.internal("\Sounds\Click.wav"));
+        badCard1 = Gdx.audio.newSound(Gdx.files.internal("\Sounds\Chainsaw_1.wav"));
+        badCard2 = Gdx.audio.newSound(Gdx.files.internal("\Sounds\Chainsaw_2.wav"));
+        goodCard1 = Gdx.audio.newSound(Gdx.files.internal("\Sounds\Birds_1.wav"));
+        goodCard2 = Gdx.audio.newSound(Gdx.files.internal("\Sounds\Birds_2.wav"));
+        mainTheme1 = Gdx.audio.newSound(Gdx.files.internal("\Sounds\MainTheme_1.wav"));
+        mainTheme2 = Gdx.audio.newSound(Gdx.files.internal("\Sounds\MainTheme_2.wav"));
+        mainMenu = Gdx.audio.newSound(Gdx.files.internal("\Sounds\MainMenu_1.wav"));
     }
 
     public static SoundManager getInstance()
